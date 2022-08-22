47834392c6cca32219220c80b6cb2fe7596eccdf
andrespbt <andrespbt14@hotmail.com>
2022-08-22 15:58:00 -0300

    Javascript modulo 2 resuelto

---
+++ Javascript/modulo2-Sintaxis/index.js
@@ -0,0 +1,10 @@
+let nombre = "Andres";
+let edad = 27;
+let desarrollador = true;
+let nacimiento = new Date(1994, 9, 2);
+let libroFavorito = {
+  titulo: "The Da Vinci Code",
+  fecha: "2003",
+  autor: "Dan Brown",
+  url: "https://www.amazon.com/Vinci-Code-Dan-Brown/dp/0593055810",
+};
